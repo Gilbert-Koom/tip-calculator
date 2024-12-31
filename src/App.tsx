bd0d23b4cb401a00ea0023051906cc6bebde6f61
Gilbert Koomson <gilbertkoomson00@gmail.com>
2024-12-30 23:04:02 -0800

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Button } from "./components/ui/button"
 
 function App() {
 
   return (
     <>
     <p className="text-red-950">hi</p>
+    <Button>hello ama ghana</Button>
     </>
   )
 }
