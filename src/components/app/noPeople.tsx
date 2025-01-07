e139ec4619d7796bf1c2a389e5c2d69886356fea
Gilbert Koomson <gilbertkoomson00@gmail.com>
2025-01-06 20:16:43 -0800

    changed the text direction

--- src/components/app/noPeople.tsx
+++ src/components/app/noPeople.tsx
@@ -35,7 +35,7 @@ function People() {
       </div>
       <div className="relative inline-block w-full">
         <Input type="number" 
-          className="bg-[#f3f8fb] rounded-[5px] w-full"
+          className="text-right bg-[#f3f8fb] rounded-[5px] w-full"
           onChange={(e)=>updater(parseFloat(e.target.value))}
           value={people}
           min={0}
